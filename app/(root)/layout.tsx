37fd2bbe6def55ff804de0e326041894ef976b8b
James Randolph <james.randolph2016@gmail.com>
2024-06-06 11:29:17 -0400

    add public assets. begin left sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,4 @@
-
+import LeftSidebar from "../../components/LeftSidebar";
 
 export default function RootLayout({
   children,
@@ -8,7 +8,7 @@ export default function RootLayout({
   return (
     <div>
         <main>
-            <p className="text-white-1">Left sidebar</p>
+            <LeftSidebar />
             {children}
             <p className="text-white-1">Right sidebar</p>
         </main>
---
+++ components/LeftSidebar.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Link from '../node_modules/next/link'
+import Image from '../node_modules/next/image'
+
+const LeftSidebar = () => {
+    return (
+        <section className="left_sidebar">
+            <nav className="flex flex-col gap-6">
+                <Link href="/" className="flex cursor-pointer items-center gap-1 pb-10 max-lg:justify-center">
+                    <Image src="/icons/logo.svg" alt="logo" width={23} height={27} />
+                    <h1 className="text-24 font-extrabold text-white max-lg:higgen">caster</h1>
+                </Link>
+
+                {[
+                    {
+                        route: '/profile',
+                        label: 'Profile',
+                        imgURL: '/icons/microphone.svg'
+                    },
+                    {
+                        route: '/home',
+                        label: 'Home',
+                        imgURL: '/icons/home.svg'
+                    }
+                ].map(({route, label}) => {
+                    return <Link href={route}>
+                        {label}
+                    </Link>
+                })}
+
+            </nav>
+        </section>
+    )
+}
+
+export default LeftSidebar
