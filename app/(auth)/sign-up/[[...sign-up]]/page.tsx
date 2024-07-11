101a80af51383cae1012247158dbde2a1bb971ef
James Randolph <james.randolph2016@gmail.com>
2024-07-11 11:36:37 -0400

    add create podcast page

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import { SignUp } from '@clerk/next.js'
+import { SignUp } from '@clerk/nextjs'
 
 const Page = () => {
     return (
         <div className="flex-center glassmorphism-auth h-screen w-full">
             <SignUp />
-            
         </div>
     )
 }
